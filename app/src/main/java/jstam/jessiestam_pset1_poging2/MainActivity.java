ac59e84105b558093fad411cdd4d80f3d87cc466
JessieStam <jessiestam@quicknet.nl>
2016-09-06 14:45:06 +0200

    Interface completed for vertical

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -2,6 +2,7 @@ package jstam.jessiestam_pset1_poging2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // when checkbox is clicked, change image visibility
+    public void CheckButton(View view) {
+
+
     }
 }
