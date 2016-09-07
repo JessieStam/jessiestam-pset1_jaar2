3fd81f9255ef6f2293fa3de2e209a1a3fc0ef5ff
JessieStam <jessiestam@quicknet.nl>
2016-09-07 22:41:21 +0200

    First setup for code

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -3,6 +3,8 @@ package jstam.jessiestam_pset1_poging2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +15,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // when checkbox is clicked, change image visibility
-    public void CheckButton(View view) {
+    public void CheckButton(View viewCheckbox) {
+
+        boolean checked = ((CheckBox) viewCheckbox).isChecked();
+
+        // get id and make string for clicked checkbox
+        // Find imageview by id(string) -> imageview id
+        // if imageview string is checked -> visible, else -> invisible
+
+        // get id and make string for clicked checkbox to match imageview id name
+        String bodyPartBox = getResources().getResourceEntryName(viewCheckbox.getId())
+                .replace("_check", "");
+
+        //String title = bodyPartBox.replace("_check", "");
+//        String id = getResources().getIdentifier(bodyPartBox, "string", getPackageName());
+//        String string = getString(id);
+//
+
+        int imageId = getResources().getIdentifier(bodyPartBox, "string", getPackageName());
+        ImageView bodyPartImage = (ImageView) findViewById(imageId);
 
 
 
+
+        // get id for clicked checkbox
+        switch(viewCheckbox.getId()) {
+            // if checkbox is checked make image visible, of not checked make image invisible
+            case R.id.check_hat:
+                ImageView Hat = (ImageView) findViewById(R.id.hat_id);
+                if (checked)
+                    Hat.setVisibility(View.VISIBLE);
+                else
+                    Hat.setVisibility(View.INVISIBLE);
+                break;
+
+            //        int getIdentifier (String name,
+//                String defType,
+//                String defPackage)
+//
+//        eyebrows_id_check;
+//        shoes_id;
+//
+//        ImageView bodyPartImage = (ImageView) findViewById(R.id.bodyPartBox);
     }
 }
