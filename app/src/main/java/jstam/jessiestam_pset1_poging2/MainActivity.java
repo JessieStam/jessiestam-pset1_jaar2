0dd0546ea976f6b2ba5db0df7dd02c71cbad556b
JessieStam <jessiestam@quicknet.nl>
2016-09-07 23:17:16 +0200

    First completed code - bug fix

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -17,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
     // when checkbox is clicked, change image visibility
     public void CheckButton(View viewCheckbox) {
 
-        // create boolean to check when checkbox is checked
-        boolean checked = ((CheckBox) viewCheckbox).isChecked();
-
-
         // get checkbox id and make string for clicked checkbox name to match ImageView id name
         String bodyPartBox = getResources().getResourceEntryName(viewCheckbox.getId())
                 .replace("_check", "");
@@ -30,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView bodyPartImage = (ImageView) findViewById(imageId);
 
         // if checkbox is checked set ImageView to visible, if not checked set to invisible
-        if (checked)
+        if (bodyPartImage.getVisibility() != View.VISIBLE)
             bodyPartImage.setVisibility(View.VISIBLE);
         else
             bodyPartImage.setVisibility(View.INVISIBLE);
