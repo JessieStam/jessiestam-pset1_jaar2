273db61b88b1001bed7634ab9026ee04bee1b68e
JessieStam <jessiestam@quicknet.nl>
2016-09-10 21:40:20 +0200

    Completed code - doesn't save yet on rotation (wrote some comments)

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -12,31 +12,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // check if it's a new Bundle or a restored one
+        if (savedInstanceState != null) {
+
+            //restore (Check which boxes are checked and change visibility)
+
+        }
+        else {
+            //do nothing??
+        }
+
     }
 
     // when checkbox is clicked, change image visibility
     public void CheckButton(View viewCheckbox) {
 
+        // create boolean that determines if a checkbox is checked
         boolean checked = ((CheckBox) viewCheckbox).isChecked();
 
         // get checkbox id and make string for clicked checkbox name to match ImageView id name
         String bodyPartBox = getResources().getResourceEntryName(viewCheckbox.getId())
                 .replace("_check", "");
 
-        // find matching ImageView by creating integer for checkbox id
+        // find matching ImageView by creating integer for ImageView id
         Integer imageId = getResources().getIdentifier(bodyPartBox, "id", getPackageName());
         ImageView bodyPartImage = (ImageView) findViewById(imageId);
 
+        // use boolean to determine if visibility of ImageView should on or of and change it
         if (checked)
             bodyPartImage.setVisibility(View.VISIBLE);
         else
             bodyPartImage.setVisibility(View.INVISIBLE);
-
-
-        // if checkbox is checked set ImageView to visible, if not checked set to invisible
-//        if (bodyPartImage != null && bodyPartImage.getVisibility() != View.VISIBLE)
-//            bodyPartImage.setVisibility(View.VISIBLE);
-//        else if (bodyPartImage != null)
-//            bodyPartImage.setVisibility(View.INVISIBLE);
     }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle savedInstanceState) {
+//        super.onSaveInstanceState(savedInstanceState);
+//
+//        // save boolean checked??
+//        // savedInstanceState.putBoolean("checked", true);
+//
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//
+//        // check welke boxes zijn gecheckt en verander ze
+//
+//    }
 }
