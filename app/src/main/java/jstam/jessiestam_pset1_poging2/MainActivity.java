1f831730abc8714621b88f961ae40f826822f2a0
JessieStam <jessiestam@quicknet.nl>
2016-09-11 18:37:55 +0200

    Completed code - first comments

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -20,15 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // if savedInstanceState is empty, create new ArrayList for checked checkboxes
+        // if savedInstanceState is empty, create new ArrayList to put IDs of checked CheckBoxes
         if (savedInstanceState == null) {
             checked_images_list = new ArrayList<>();
         }
-        // if savedInstanceState is restores, load list of checked checkboxes
+        // if savedInstanceState is full, load list of checked CheckBox IDs
         else {
             checked_images_list = savedInstanceState.getIntegerArrayList("savedList");
 
-            // iterate over list of checked checkboxes
+            // iterate over list of checked CheckBox IDs
             if (checked_images_list != null) {
                 // change each corresponding ImageView to visible
                 for (Integer list_item : checked_images_list) {
@@ -40,37 +40,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * when checkbox is clicked, change ImageView visibility
+     * when CheckBox is clicked, change ImageView visibility
      */
     public void CheckButton(View viewCheckbox) {
 
-        // create boolean that determines if a checkbox is checked
+        // create boolean that determines if CheckBox is checked
         boolean checked = ((CheckBox) viewCheckbox).isChecked();
 
-        // get checkbox id and make string for clicked checkbox name to match ImageView id
+        // get CheckBox ID and make String for clicked CheckBox name to match ImageView ID
         String bodyPartBox = getResources().getResourceEntryName(viewCheckbox.getId())
                 .replace("_check", "");
 
-        // find matching ImageView by creating integer for ImageView id
+        // find matching ImageView by creating Integer for ImageView ID
         imageId = getResources().getIdentifier(bodyPartBox, "id", getPackageName());
         bodyPartImage = (ImageView) findViewById(imageId);
 
-        // use boolean to determine if visibility of ImageView should on or of and change it
+        // use boolean to determine if ImageView should be (in)visible and change visibility
         if (checked) {
             bodyPartImage.setVisibility(View.VISIBLE);
 
-            //save imageId in list
+            // save CheckBox ID corresponding to visible ImageView in list
             checked_images_list.add(imageId);
         }
         else {
             bodyPartImage.setVisibility(View.INVISIBLE);
 
-            // remove imageId from list
+            // create Iterator to iterate over list of CheckBox IDs
             Iterator<Integer> remove_iterator = checked_images_list.iterator();
+
+            // iterate over IDs in list to find CheckBox ID to be removed
             while (remove_iterator.hasNext()) {
-                //check_iterator.next();
                 Integer list_item = remove_iterator.next();
-                // compare list_item to imageId
+
+                // remove CheckBox ID corresponding to invisible ImageView from list
                 if (list_item.equals(imageId))
                     remove_iterator.remove();
             }
@@ -81,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        // save ArrayList of visible images
+        // save ArrayList of visible ImageViews
         outState.putIntegerArrayList("savedList", checked_images_list);
     }
 
@@ -89,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        // check welke boxes zijn gecheckt en verander ze
+        // restore ArrayList of visible ImageViews
         checked_images_list = savedInstanceState.getIntegerArrayList("savedList");
     }
 }
