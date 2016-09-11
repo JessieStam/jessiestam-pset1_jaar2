886e0c10e1a065e961231254f3b24194b36be603
JessieStam <jessiestam@quicknet.nl>
2016-09-11 18:13:05 +0200

    Firt code for saving data on screen rotation

--- app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
+++ app/src/main/java/jstam/jessiestam_pset1_poging2/MainActivity.java
@@ -6,60 +6,90 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Integer> checked_images_list;
+    ImageView bodyPartImage;
+    Integer imageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // check if it's a new Bundle or a restored one
-        if (savedInstanceState != null) {
-
-            //restore (Check which boxes are checked and change visibility)
-
+        // if savedInstanceState is empty, create new ArrayList for checked checkboxes
+        if (savedInstanceState == null) {
+            checked_images_list = new ArrayList<>();
         }
+        // if savedInstanceState is restores, load list of checked checkboxes
         else {
-            //do nothing??
-        }
+            checked_images_list = savedInstanceState.getIntegerArrayList("savedList");
 
+            // iterate over list of checked checkboxes
+            if (checked_images_list != null) {
+                // change each corresponding ImageView to visible
+                for (Integer list_item : checked_images_list) {
+                    bodyPartImage = (ImageView) findViewById(list_item);
+                    bodyPartImage.setVisibility(View.VISIBLE);
+                }
+            }
+        }
     }
 
-    // when checkbox is clicked, change image visibility
+    /*
+     * when checkbox is clicked, change ImageView visibility
+     */
     public void CheckButton(View viewCheckbox) {
 
         // create boolean that determines if a checkbox is checked
         boolean checked = ((CheckBox) viewCheckbox).isChecked();
 
-        // get checkbox id and make string for clicked checkbox name to match ImageView id name
+        // get checkbox id and make string for clicked checkbox name to match ImageView id
         String bodyPartBox = getResources().getResourceEntryName(viewCheckbox.getId())
                 .replace("_check", "");
 
         // find matching ImageView by creating integer for ImageView id
-        Integer imageId = getResources().getIdentifier(bodyPartBox, "id", getPackageName());
-        ImageView bodyPartImage = (ImageView) findViewById(imageId);
+        imageId = getResources().getIdentifier(bodyPartBox, "id", getPackageName());
+        bodyPartImage = (ImageView) findViewById(imageId);
 
         // use boolean to determine if visibility of ImageView should on or of and change it
-        if (checked)
+        if (checked) {
             bodyPartImage.setVisibility(View.VISIBLE);
-        else
+
+            //save imageId in list
+            checked_images_list.add(imageId);
+        }
+        else {
             bodyPartImage.setVisibility(View.INVISIBLE);
+
+            // remove imageId from list
+            Iterator<Integer> remove_iterator = checked_images_list.iterator();
+            while (remove_iterator.hasNext()) {
+                //check_iterator.next();
+                Integer list_item = remove_iterator.next();
+                // compare list_item to imageId
+                if (list_item.equals(imageId))
+                    remove_iterator.remove();
+            }
+        }
     }
 
-//    @Override
-//    protected void onSaveInstanceState(Bundle savedInstanceState) {
-//        super.onSaveInstanceState(savedInstanceState);
-//
-//        // save boolean checked??
-//        // savedInstanceState.putBoolean("checked", true);
-//
-//    }
-//
-//    @Override
-//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-//        super.onRestoreInstanceState(savedInstanceState);
-//
-//        // check welke boxes zijn gecheckt en verander ze
-//
-//    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save ArrayList of visible images
+        outState.putIntegerArrayList("savedList", checked_images_list);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // check welke boxes zijn gecheckt en verander ze
+        checked_images_list = savedInstanceState.getIntegerArrayList("savedList");
+    }
 }
